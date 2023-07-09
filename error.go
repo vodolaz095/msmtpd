232eef15dcdadf0ce8b76558e6fa892192b9eb64
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2023-07-09 13:27:40 +0300

    more errors

--- error.go
+++ error.go
@@ -19,3 +19,24 @@ func (e ErrorSMTP) Error() string {
 // ErrServerClosed is returned by the Server's Serve and ListenAndServe,
 // methods after a call to shut down.
 var ErrServerClosed = errors.New("smtp: Server closed")
+
+// Status codes for SMTP negotiations
+// see https://en.wikipedia.org/wiki/List_of_SMTP_server_return_codes
+
+// ErrServiceNotAvailable means server cannot perform this SMTP transaction and it should be retried later
+var ErrServiceNotAvailable = ErrorSMTP{
+	Code:    421,
+	Message: "Service not available, closing transmission channel. Try again later, please.",
+}
+
+// ErrServiceDoesNotAcceptEmail means server will not perform this SMTP transaction, even if your try to retry it
+var ErrServiceDoesNotAcceptEmail = ErrorSMTP{
+	Code:    521,
+	Message: "Server does not accept mail. Do not retry delivery, please. It will fail.",
+}
+
+// ErrAuthenticationCredentialsInvalid means SMTP credentials are invalid
+var ErrAuthenticationCredentialsInvalid = ErrorSMTP{
+	Code:    535,
+	Message: "Authentication credentials are invalid.",
+}
