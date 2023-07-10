f71fdbf7204e20e91772e3255db88854fb2e0d24
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2023-07-10 22:58:38 +0300

    check ptr for helo

---
+++ plugins/helo/check_helo.go
@@ -0,0 +1,95 @@
+package helo
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	"msmtpd"
+)
+
+type Options struct {
+	// TolerateInvalidHostname allows HELO/EHLO argument to be invalid hostname
+	TolerateInvalidHostname bool
+	// TolerateBareIP allows HELO/EHLO argument to be bare IP
+	TolerateBareIP bool
+	// TolerateDynamic allows HELO/EHLO argument to include parts of connection IP address
+	TolerateDynamic bool
+	// TolerateRDNSMismatch reverseDNS server name of connecting IP to be different, than HELO provided
+	TolerateRDNSMismatch bool
+}
+
+const complain = "I don't like the way you introduce yourself. Goodbye!"
+
+func CheckHELO(opts Options) msmtpd.CheckerFunc {
+	tlds := strings.Split(topListDomains, "\n")
+	return func(transaction *msmtpd.Transaction) error {
+		var pass bool
+		if !opts.TolerateInvalidHostname {
+			fixed := strings.ToUpper(transaction.HeloName)
+			for i := range tlds {
+				if pass {
+					continue
+				}
+				if strings.HasSuffix(fixed, "."+tlds[i]) {
+					pass = true
+				}
+			}
+			if !pass {
+				transaction.LogWarn("HELO/EHLO hostname %s is invalid", transaction.HeloName)
+				return msmtpd.ErrorSMTP{
+					Code:    521,
+					Message: complain,
+				}
+			}
+			transaction.LogDebug("HELO/EHLO %s seems to be in top domain list", transaction.HeloName)
+		}
+		if !opts.TolerateBareIP {
+			if net.ParseIP(transaction.HeloName) != nil {
+				transaction.LogWarn("HELO/EHLO hostname %s is base ip", transaction.HeloName)
+				return msmtpd.ErrorSMTP{
+					Code:    521,
+					Message: complain,
+				}
+			}
+			transaction.LogDebug("HELO/EHLO %s seems to be not bare IP", transaction.HeloName)
+		}
+		if !opts.TolerateDynamic {
+			// TODO: implement
+			transaction.LogWarn("HELO/EHLO hostname %s looks dynamic", transaction.HeloName)
+			return msmtpd.ErrorSMTP{
+				Code:    521,
+				Message: complain,
+			}
+		}
+		if !opts.TolerateRDNSMismatch {
+			pass = false
+			needle := transaction.Addr.(*net.TCPAddr).IP.String()
+			transaction.LogDebug("Resolving PTR for %s...", needle)
+			ptr, err := transaction.Resolver().LookupAddr(transaction.Context(), needle)
+			if err != nil {
+				transaction.LogError(err, fmt.Sprintf("temporary error in PTR resolution for %s",
+					transaction.Addr.String()))
+				return msmtpd.ErrorSMTP{
+					Code:    421,
+					Message: "I cannot validate your HELO/EHLO right now, please, try again later!",
+				}
+			}
+			for i := range ptr {
+				transaction.LogDebug("For %s PTR %s is resolved", needle, ptr[i])
+				if ptr[i] == transaction.HeloName+"." {
+					pass = true
+				}
+			}
+			if !pass {
+				transaction.LogWarn("For HELO/EHLO %s there is no matching PTR records", transaction.HeloName)
+				return msmtpd.ErrorSMTP{
+					Code:    521,
+					Message: complain,
+				}
+			}
+			transaction.LogDebug("HELO/EHLO %s is matching RDNS record", transaction.HeloName)
+		}
+		return nil
+	}
+}
---
+++ plugins/helo/check_helo_test.go
@@ -0,0 +1,151 @@
+package helo
+
+import (
+	"fmt"
+	"net"
+	"net/smtp"
+	"testing"
+
+	"msmtpd"
+)
+
+var generalError = fmt.Errorf("521 %s", complain)
+
+type testCase struct {
+	HELO  string
+	Error error
+}
+
+func TestCheckHELO_Dynamic(t *testing.T) {
+	cases := []testCase{
+		{
+			HELO:  "Sodom",
+			Error: generalError,
+		},
+		{
+			HELO:  "193.41.76.125",
+			Error: generalError,
+		},
+		{
+			HELO:  "R193-41-76-125.utex-telecom.ru",
+			Error: nil, // important!
+		},
+		{
+			HELO:  "mail.example.org",
+			Error: generalError,
+		},
+	}
+	addr, closer := runserver(t, &msmtpd.Server{
+		ConnectionCheckers: []msmtpd.CheckerFunc{
+			func(transaction *msmtpd.Transaction) error {
+				transaction.Addr = &net.TCPAddr{
+					IP:   net.ParseIP("193.41.76.125"),
+					Port: 49762,
+				}
+				return nil
+			},
+		},
+		HeloCheckers: []msmtpd.CheckerFunc{
+			CheckHELO(Options{
+				TolerateInvalidHostname: false,
+				TolerateBareIP:          false,
+				TolerateDynamic:         true, // so i can actually ran this test with familiar for me IP address
+				TolerateRDNSMismatch:    false,
+			}),
+		},
+	})
+	defer closer()
+	for k := range cases {
+		c, err := smtp.Dial(addr)
+		if err != nil {
+			t.Errorf("Dial failed: %v", err)
+		}
+		err = c.Hello(cases[k].HELO)
+		if err != nil {
+			if cases[k].Error != nil {
+				if err.Error() == cases[k].Error.Error() {
+					t.Logf("%s - FAILED AS EXPECTED", cases[k].HELO)
+				} else {
+					t.Errorf("wrong error checking - %s. Expected: %s. Received: %s",
+						cases[k].HELO, cases[k].Error, err)
+
+				}
+			} else {
+				t.Errorf("unexpected error checking >>>%s<<< - %s", cases[k].HELO, err)
+			}
+		} else {
+			if cases[k].Error != nil {
+				t.Errorf("expected error `%s` was not thrown while checking >>>%s<<< - ",
+					cases[k].Error, cases[k].HELO)
+			}
+		}
+		c.Quit() // it is ok
+	}
+}
+
+func TestCheckHELO_Default(t *testing.T) {
+	cases := []testCase{
+		{
+			HELO:  "Sodom",
+			Error: generalError,
+		},
+		{
+			HELO:  "193.41.76.125",
+			Error: generalError,
+		},
+		{
+			HELO:  "R193-41-76-125.utex-telecom.ru",
+			Error: generalError,
+		},
+		{
+			HELO:  "mail.example.org",
+			Error: generalError,
+		},
+	}
+	addr, closer := runserver(t, &msmtpd.Server{
+		ConnectionCheckers: []msmtpd.CheckerFunc{
+			func(transaction *msmtpd.Transaction) error {
+				transaction.Addr = &net.TCPAddr{
+					IP:   net.ParseIP("193.41.76.125"),
+					Port: 49762,
+				}
+				return nil
+			},
+		},
+		HeloCheckers: []msmtpd.CheckerFunc{
+			CheckHELO(Options{
+				TolerateInvalidHostname: false,
+				TolerateBareIP:          false,
+				TolerateDynamic:         false,
+				TolerateRDNSMismatch:    false,
+			}),
+		},
+	})
+	defer closer()
+	for k := range cases {
+		c, err := smtp.Dial(addr)
+		if err != nil {
+			t.Errorf("Dial failed: %v", err)
+		}
+		err = c.Hello(cases[k].HELO)
+		if err != nil {
+			if cases[k].Error != nil {
+				if err.Error() == cases[k].Error.Error() {
+					t.Logf("%s - FAILED AS EXPECTED", cases[k].HELO)
+				} else {
+					t.Errorf("wrong error checking - %s. Expected: %s. Received: %s",
+						cases[k].HELO, cases[k].Error, err)
+
+				}
+			} else {
+				t.Errorf("unexpected error checking >>>%s<<< - %s", cases[k].HELO, err)
+			}
+		} else {
+			if cases[k].Error != nil {
+				t.Errorf("expected error `%s` was not thrown while checking >>>%s<<< - ",
+					cases[k].Error, cases[k].HELO)
+			}
+		}
+		c.Quit() // it is ok
+	}
+}
