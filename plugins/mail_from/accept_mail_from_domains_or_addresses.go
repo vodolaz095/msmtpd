0ab3b944477e9ec675849afd823ee13952f77f64
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2023-07-11 23:28:08 +0300

    plugins/mail_from.AcceptMailFromDomainsOrAddresses

---
+++ plugins/mail_from/accept_mail_from_domains_or_addresses.go
@@ -0,0 +1,48 @@
+package mail_from
+
+import (
+	"log"
+	"net/mail"
+	"strings"
+
+	"msmtpd"
+)
+
+// AcceptMailFromDomainsOrAddresses allows senders either from one of whilelisted domain, or from one of whitelisted addresses.
+// It is more complicated version of AcceptMailFromDomains and AcceptMailFromAddresses.
+func AcceptMailFromDomainsOrAddresses(whitelistedDomains, whitelistedAddresses []string) msmtpd.CheckerFunc {
+	var err error
+	var parsed *mail.Address
+	goodMailFroms := make(map[mail.Address]bool, 0)
+
+	for i, raw := range whitelistedAddresses {
+		parsed, err = mail.ParseAddress(raw)
+		if err != nil {
+			log.Fatalf("%s : while plugin mail_from/AcceptMailFromDomainsOrAddresses tries to parse address %v %s",
+				err, i, raw,
+			)
+		}
+		goodMailFroms[*parsed] = true
+	}
+	goodDomains := make(map[string]bool, 0)
+	for _, raw := range whitelistedDomains {
+		goodDomains[strings.ToLower(raw)] = true
+	}
+	return func(transaction *msmtpd.Transaction) error {
+		domain := strings.Split(transaction.MailFrom.Address, "@")[1]
+		_, found := goodDomains[domain]
+		if found {
+			transaction.LogDebug("Sender's %s domain is whitelisted", transaction.MailFrom.String())
+			return nil
+		}
+		_, found = goodMailFroms[transaction.MailFrom]
+		if found {
+			transaction.LogDebug("Sender %s is whitelisted", transaction.MailFrom.String())
+			return nil
+		}
+		return msmtpd.ErrorSMTP{
+			Code:    521,
+			Message: "I'm sorry, but your email address is not in whitelist",
+		}
+	}
+}
--- plugins/mail_from/whitelist_test.go
+++ plugins/mail_from/whitelist_test.go
@@ -93,3 +93,57 @@ func TestAcceptMailFromAddresses(t *testing.T) {
 		}
 	}
 }
+
+func TestAcceptMailFromDomainsOrAddresses(t *testing.T) {
+	cases := make(map[string]error, 0)
+
+	cases["thisIsNotAEmail"] = fmt.Errorf("502 Malformed e-mail address")
+	cases["a@example.org"] = nil
+	cases["a@vodolaz095.ru"] = nil
+	cases["a@gmail.com"] = fmt.Errorf("521 I'm sorry, but your email address is not in whitelist")
+	cases["b@gmail.com"] = fmt.Errorf("521 I'm sorry, but your email address is not in whitelist")
+
+	cases["a@gmail.com"] = nil
+	cases["b@gmail.com"] = nil
+	cases["d@gmail.com"] = fmt.Errorf("521 I'm sorry, but your email address is not in whitelist")
+	cases["e@gmail.com"] = fmt.Errorf("521 I'm sorry, but your email address is not in whitelist")
+	cases["info@yandex.ru"] = fmt.Errorf("521 I'm sorry, but your email address is not in whitelist")
+
+	addr, closer := runserver(t, &msmtpd.Server{
+		SenderCheckers: []msmtpd.CheckerFunc{
+			AcceptMailFromDomainsOrAddresses(
+				[]string{
+					"example.org",
+					"vodolaz095.ru",
+				},
+				[]string{
+					"a@gmail.com",
+					"b@gmail.com",
+				}),
+		},
+	})
+	defer closer()
+	for k, v := range cases {
+		c, err := smtp.Dial(addr)
+		if err != nil {
+			t.Errorf("Dial failed: %v", err)
+		}
+		if err = c.Hello("localhost"); err != nil {
+			t.Errorf("HELO failed: %v", err)
+		}
+		err = c.Mail(k)
+		if err != nil {
+			if v != nil {
+				if err.Error() != v.Error() {
+					t.Errorf("wrong error checking %s. Expected: %s. Received: %s", k, v, err)
+				}
+				continue
+			}
+			t.Errorf("unexpected error checking %s - %s", k, err)
+		}
+		err = c.Quit()
+		if err != nil {
+			t.Errorf("%s : while closing connection", err)
+		}
+	}
+}
