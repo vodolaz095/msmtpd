8c3646df942f36f45186a12916151658446c7aa7
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2023-07-25 22:48:52 +0300

    blacklists

--- plugins/connection/blacklist.go
+++ plugins/connection/blacklist.go
@@ -1,17 +1,51 @@
 package connection
 
 import (
+	"strings"
+
 	"msmtpd"
 )
 
 func Whitelist(ipAddressesToAccept []string) func(transaction *msmtpd.Transaction) error {
 	return func(transaction *msmtpd.Transaction) error {
-		return nil
+		var found bool
+		for i := range ipAddressesToAccept {
+			found = strings.HasPrefix(transaction.Addr.String(), ipAddressesToAccept[i])
+			if found {
+				transaction.LogInfo("IP address %s is whitelisted by rule %s",
+					transaction.Addr.String(), ipAddressesToAccept[i],
+				)
+				break
+			}
+		}
+		if found {
+			return nil
+		}
+		return msmtpd.ErrorSMTP{
+			Code:    521,
+			Message: "FUCK OFF!", // lol
+		}
 	}
 }
 
 func Blacklist(ipAddressesToBlock []string) func(transaction *msmtpd.Transaction) error {
 	return func(transaction *msmtpd.Transaction) error {
+		var found bool
+		for i := range ipAddressesToBlock {
+			found = strings.HasPrefix(transaction.Addr.String(), ipAddressesToBlock[i])
+			if found {
+				transaction.LogInfo("IP address %s is blacklisted by rule %s",
+					transaction.Addr.String(), ipAddressesToBlock[i],
+				)
+				break
+			}
+		}
+		if found {
+			return msmtpd.ErrorSMTP{
+				Code:    521,
+				Message: "FUCK OFF!", // lol
+			}
+		}
 		return nil
 	}
 }
