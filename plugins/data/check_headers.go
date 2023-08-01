04dc398dc16aa9ee6113911d77736b8b2677e19a
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2023-08-02 00:08:14 +0300

    plugin/data.CheckHeaders is more RFC 5322 compliant

--- plugins/data/check_headers.go
+++ plugins/data/check_headers.go
@@ -1,36 +1,119 @@
 package data
 
 import (
+	"time"
+
 	"github.com/vodolaz095/msmtpd"
 )
 
 // Good read - https://en.wikipedia.org/wiki/List_of_SMTP_server_return_codes
 
+// All messages MUST have a 'Date' and 'From' header and a message may not
+// contain more than one 'Date', 'From', 'Sender', 'Reply-To', 'To', 'Cc', 'Bcc',
+// 'Message-Id', 'In-Reply-To', 'References' or 'Subject' header.
+// (c) RFC 5322
+
 const complain = "I cannot parse your message. Do not send me this particular message in future, please, i will never accept it. Thanks in advance!"
 
 // DefaultHeadersToRequire are headers we expect to exist in any incoming email message
 var DefaultHeadersToRequire = []string{
-	"To",
-	"From",
-	"Message-ID",
 	"Date",
+	"From",
+
+	"To",
+	"Message-ID",
 	"Subject",
 }
 
-// CheckHeaders is Handler for processing message body to ensure it contains minimal headers required
-func CheckHeaders(headersRequired []string) func(transaction *msmtpd.Transaction) error {
+// mandatoryHeaders are headers should always be present in email message according to RFC 5322
+var mandatoryHeaders = []string{
+	"Date",
+	"From",
+}
+
+// uniqueHeaders are headers that should not have duplicates according to RFC 5322
+var uniqueHeaders = []string{
+	"Date",
+	"From",
+	"Sender",
+	"Reply-To",
+	"To",
+	"Cc",
+	"Bcc",
+	"Message-Id",
+	"In-Reply-To",
+	"References",
+	"Subject",
+}
+
+const tooOld = 15 * 24 * time.Hour
+const tooFarInFuture = 2 * 24 * time.Hour
+
+// CheckHeaders is DataChecker for processing message body to ensure it is compatible with RFC 5322 and
+// contains minimal headers required. This checker protects from the majority of malformed messages
+// that can break, for example, antique Outlook Express and probably other email clients.
+func CheckHeaders(headersRequired []string) msmtpd.DataChecker {
 	return func(transaction *msmtpd.Transaction) error {
 		var val string
-		for _, header := range headersRequired {
+		for _, header := range mandatoryHeaders {
 			val = transaction.Parsed.Header.Get(header)
 			if val == "" {
-				transaction.LogWarn("header %s is missing", header)
+				transaction.LogWarn("mandatory header %s is missing", header)
 				return msmtpd.ErrorSMTP{
 					Code:    521,
 					Message: complain,
 				}
 			}
 			transaction.LogDebug("Header %s is %s", header, val)
+		}
+		for _, header := range headersRequired {
+			val = transaction.Parsed.Header.Get(header)
+			if val == "" {
+				transaction.LogWarn("required header %s is missing", header)
+				return msmtpd.ErrorSMTP{
+					Code:    521,
+					Message: complain,
+				}
+			}
+			transaction.LogDebug("Header %s is %s", header, val)
+		}
+		for _, header := range uniqueHeaders {
+			parts, found := transaction.Parsed.Header[header]
+			if found {
+				if len(parts) > 1 {
+					transaction.LogWarn("Duplicate header %s %v is found",
+						header, parts,
+					)
+					return msmtpd.ErrorSMTP{
+						Code:    521,
+						Message: complain,
+					}
+				}
+			}
+		}
+		timestamp, err := transaction.Parsed.Header.Date()
+		if err != nil {
+			transaction.LogWarn("Malformed date header with value %s",
+				transaction.Parsed.Header.Get("Date"))
+			return msmtpd.ErrorSMTP{
+				Code:    521,
+				Message: complain,
+			}
+		}
+		transaction.LogInfo("Message was generated on %s", timestamp.Format(time.ANSIC))
+		if time.Since(timestamp) > tooOld {
+			transaction.LogWarn("Message is too old: %s", time.Since(timestamp).String())
+			return msmtpd.ErrorSMTP{
+				Code:    521,
+				Message: complain,
+			}
+		}
+		if time.Now().Add(tooFarInFuture).Before(timestamp) {
+			transaction.LogWarn("Message is too far away in future")
+			return msmtpd.ErrorSMTP{
+				Code:    521,
+				Message: complain,
+			}
 		}
 		transaction.LogInfo("Headers are in place!")
 		return nil
--- plugins/data/check_headers_test.go
+++ plugins/data/check_headers_test.go
@@ -1,23 +1,23 @@
 package data
 
 import (
+	"bytes"
 	"fmt"
 	"net/smtp"
 	"testing"
+	"time"
 
 	"github.com/vodolaz095/msmtpd"
 )
 
 func TestBodyParseAndCheckHeadersOK(t *testing.T) {
-	validMessage := `Date: Sun, 11 Jun 2023 19:49:29 +0300
-To: scuba@vodolaz095.ru
-From: scuba@vodolaz095.ru
-Subject: test Sun, 11 Jun 2023 19:49:29 +0300
-Message-Id: <20230611194929.017435@localhost>
-X-Mailer: swaks v20190914.0 jetmore.org/john/code/swaks/
-
-This is a test mailing
-`
+	buf := bytes.NewBufferString("")
+	fmt.Fprintf(buf, "Date: %s\n", time.Now().Format(time.RFC1123Z))
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	buf.WriteString("From: scuba@vodolaz095.ru\n")
+	buf.WriteString("Subject: test with all headers ok\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing")
 
 	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
 		DataCheckers: []msmtpd.DataChecker{
@@ -42,7 +42,7 @@ This is a test mailing
 	if err != nil {
 		t.Errorf("Data failed: %v", err)
 	}
-	_, err = fmt.Fprintf(wc, validMessage)
+	_, err = fmt.Fprintf(wc, buf.String())
 	if err != nil {
 		t.Errorf("Data body failed: %v", err)
 	}
@@ -93,15 +93,14 @@ func TestBodyParseAndCheckHeadersMalformed(t *testing.T) {
 	t.Errorf("error not thrown")
 }
 
-func TestBodyParseAndCheckHeadersMissingHeaders(t *testing.T) {
-	messageWithoutFrom := `Date: Sun, 11 Jun 2023 19:49:29 +0300
-To: scuba@vodolaz095.ru
-Subject: test Sun, 11 Jun 2023 19:49:29 +0300
-Message-Id: <20230611194929.017435@localhost>
-X-Mailer: swaks v20190914.0 jetmore.org/john/code/swaks/
-
-This is a test mailing
-`
+func TestBodyParseAndCheckHeadersMissingMandatoryHeaderFrom(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	fmt.Fprintf(buf, "Date: %s\n", time.Now().Format(time.RFC1123Z))
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	// buf.WriteString("From: scuba@vodolaz095.ru\n") // IMPORTANT
+	buf.WriteString("Subject: from not present\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing without FROM header")
 
 	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
 		DataCheckers: []msmtpd.DataChecker{
@@ -126,7 +125,296 @@ This is a test mailing
 	if err != nil {
 		t.Errorf("Data failed: %v", err)
 	}
-	_, err = fmt.Fprintf(wc, messageWithoutFrom)
+	_, err = fmt.Fprintf(wc, buf.String())
+	if err != nil {
+		t.Errorf("Data body failed: %v", err)
+	}
+	err = wc.Close()
+	if err != nil {
+		if err.Error() == "521 I cannot parse your message. Do not send me this particular message in future, please, i will never accept it. Thanks in advance!" {
+			t.Logf("proper error is thrown")
+			return
+		} else {
+			t.Errorf("Data close failed with wrong error %v", err)
+		}
+	}
+	t.Errorf("error not thrown")
+}
+
+func TestBodyParseAndCheckHeadersMissingMandatoryHeaderDate(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	//	fmt.Fprintf(buf, "Date: %s\n", time.Now().Format(time.RFC1123Z)) // IMPORTANT
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	buf.WriteString("From: scuba@vodolaz095.ru\n")
+	buf.WriteString("Subject: date is missing\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing without DATE header")
+
+	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
+		DataCheckers: []msmtpd.DataChecker{
+			CheckHeaders(DefaultHeadersToRequire),
+		},
+	})
+	defer closer()
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		t.Errorf("Dial failed: %v", err)
+	}
+	if err = c.Hello("localhost"); err != nil {
+		t.Errorf("HELO failed: %v", err)
+	}
+	if err = c.Mail("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Mail failed: %v", err)
+	}
+	if err = c.Rcpt("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Rcpt failed: %v", err)
+	}
+	wc, err := c.Data()
+	if err != nil {
+		t.Errorf("Data failed: %v", err)
+	}
+	_, err = fmt.Fprintf(wc, buf.String())
+	if err != nil {
+		t.Errorf("Data body failed: %v", err)
+	}
+	err = wc.Close()
+	if err != nil {
+		if err.Error() == "521 I cannot parse your message. Do not send me this particular message in future, please, i will never accept it. Thanks in advance!" {
+			t.Logf("proper error is thrown")
+			return
+		} else {
+			t.Errorf("Data close failed with wrong error %v", err)
+		}
+	}
+	t.Errorf("error not thrown")
+}
+
+func TestBodyParseAndCheckHeadersMissingRequiredSubject(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	fmt.Fprintf(buf, "Date: %s\n", time.Now().Format(time.RFC1123Z))
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	buf.WriteString("From: scuba@vodolaz095.ru\n")
+	// buf.WriteString("Subject: subject is not mandatory, but it is missing\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing without SUBJECT header")
+
+	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
+		DataCheckers: []msmtpd.DataChecker{
+			CheckHeaders(DefaultHeadersToRequire),
+		},
+	})
+	defer closer()
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		t.Errorf("Dial failed: %v", err)
+	}
+	if err = c.Hello("localhost"); err != nil {
+		t.Errorf("HELO failed: %v", err)
+	}
+	if err = c.Mail("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Mail failed: %v", err)
+	}
+	if err = c.Rcpt("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Rcpt failed: %v", err)
+	}
+	wc, err := c.Data()
+	if err != nil {
+		t.Errorf("Data failed: %v", err)
+	}
+	_, err = fmt.Fprintf(wc, buf.String())
+	if err != nil {
+		t.Errorf("Data body failed: %v", err)
+	}
+	err = wc.Close()
+	if err != nil {
+		if err.Error() == "521 I cannot parse your message. Do not send me this particular message in future, please, i will never accept it. Thanks in advance!" {
+			t.Logf("proper error is thrown")
+			return
+		} else {
+			t.Errorf("Data close failed with wrong error %v", err)
+		}
+	}
+	t.Errorf("error not thrown")
+}
+
+func TestBodyParseAndCheckHeadersDuplicate(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	fmt.Fprintf(buf, "Date: %s\n", time.Now().Format(time.RFC1123Z))
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	buf.WriteString("From: scuba@vodolaz095.ru\n")
+	buf.WriteString("Subject: test with duplicate headers\n")
+	buf.WriteString("Subject: duplicate subject\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing with duplicate subject")
+
+	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
+		DataCheckers: []msmtpd.DataChecker{
+			CheckHeaders(DefaultHeadersToRequire),
+		},
+	})
+	defer closer()
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		t.Errorf("Dial failed: %v", err)
+	}
+	if err = c.Hello("localhost"); err != nil {
+		t.Errorf("HELO failed: %v", err)
+	}
+	if err = c.Mail("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Mail failed: %v", err)
+	}
+	if err = c.Rcpt("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Rcpt failed: %v", err)
+	}
+	wc, err := c.Data()
+	if err != nil {
+		t.Errorf("Data failed: %v", err)
+	}
+	_, err = fmt.Fprintf(wc, buf.String())
+	if err != nil {
+		t.Errorf("Data body failed: %v", err)
+	}
+	err = wc.Close()
+	if err != nil {
+		if err.Error() == "521 I cannot parse your message. Do not send me this particular message in future, please, i will never accept it. Thanks in advance!" {
+			t.Logf("proper error is thrown")
+			return
+		} else {
+			t.Errorf("Data close failed with wrong error %v", err)
+		}
+	}
+	t.Errorf("error not thrown")
+}
+
+func TestBodyParseAndCheckHeadersDateTooOld(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	fmt.Fprintf(buf, "Date: %s\n", time.Now().AddDate(-1, 1, 1).Format(time.RFC1123Z))
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	buf.WriteString("From: scuba@vodolaz095.ru\n")
+	buf.WriteString("Subject: test date too old\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing with date too old")
+
+	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
+		DataCheckers: []msmtpd.DataChecker{
+			CheckHeaders(DefaultHeadersToRequire),
+		},
+	})
+	defer closer()
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		t.Errorf("Dial failed: %v", err)
+	}
+	if err = c.Hello("localhost"); err != nil {
+		t.Errorf("HELO failed: %v", err)
+	}
+	if err = c.Mail("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Mail failed: %v", err)
+	}
+	if err = c.Rcpt("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Rcpt failed: %v", err)
+	}
+	wc, err := c.Data()
+	if err != nil {
+		t.Errorf("Data failed: %v", err)
+	}
+	_, err = fmt.Fprintf(wc, buf.String())
+	if err != nil {
+		t.Errorf("Data body failed: %v", err)
+	}
+	err = wc.Close()
+	if err != nil {
+		if err.Error() == "521 I cannot parse your message. Do not send me this particular message in future, please, i will never accept it. Thanks in advance!" {
+			t.Logf("proper error is thrown")
+			return
+		} else {
+			t.Errorf("Data close failed with wrong error %v", err)
+		}
+	}
+	t.Errorf("error not thrown")
+}
+
+func TestBodyParseAndCheckHeadersDateToFarInFuture(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	fmt.Fprintf(buf, "Date: %s\n", time.Now().AddDate(0, 1, 1).Format(time.RFC1123Z))
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	buf.WriteString("From: scuba@vodolaz095.ru\n")
+	buf.WriteString("Subject: test date in future\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing with date in future")
+
+	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
+		DataCheckers: []msmtpd.DataChecker{
+			CheckHeaders(DefaultHeadersToRequire),
+		},
+	})
+	defer closer()
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		t.Errorf("Dial failed: %v", err)
+	}
+	if err = c.Hello("localhost"); err != nil {
+		t.Errorf("HELO failed: %v", err)
+	}
+	if err = c.Mail("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Mail failed: %v", err)
+	}
+	if err = c.Rcpt("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Rcpt failed: %v", err)
+	}
+	wc, err := c.Data()
+	if err != nil {
+		t.Errorf("Data failed: %v", err)
+	}
+	_, err = fmt.Fprintf(wc, buf.String())
+	if err != nil {
+		t.Errorf("Data body failed: %v", err)
+	}
+	err = wc.Close()
+	if err != nil {
+		if err.Error() == "521 I cannot parse your message. Do not send me this particular message in future, please, i will never accept it. Thanks in advance!" {
+			t.Logf("proper error is thrown")
+			return
+		} else {
+			t.Errorf("Data close failed with wrong error %v", err)
+		}
+	}
+	t.Errorf("error not thrown")
+}
+
+func TestBodyParseAndCheckHeadersDateMalformed(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	buf.WriteString("Date: сегодня, после обеда\n") // yes
+	buf.WriteString("To: scuba@vodolaz095.ru\n")
+	buf.WriteString("From: scuba@vodolaz095.ru\n")
+	buf.WriteString("Subject: test with strange date\n")
+	buf.WriteString("Message-Id: <20230611194929.017435@localhost>\n")
+	buf.WriteString("\n\nThis is a test mailing with strange date")
+
+	addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
+		DataCheckers: []msmtpd.DataChecker{
+			CheckHeaders(DefaultHeadersToRequire),
+		},
+	})
+	defer closer()
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		t.Errorf("Dial failed: %v", err)
+	}
+	if err = c.Hello("localhost"); err != nil {
+		t.Errorf("HELO failed: %v", err)
+	}
+	if err = c.Mail("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Mail failed: %v", err)
+	}
+	if err = c.Rcpt("scuba@vodolaz095.ru"); err != nil {
+		t.Errorf("Rcpt failed: %v", err)
+	}
+	wc, err := c.Data()
+	if err != nil {
+		t.Errorf("Data failed: %v", err)
+	}
+	_, err = fmt.Fprintf(wc, buf.String())
 	if err != nil {
 		t.Errorf("Data body failed: %v", err)
 	}
