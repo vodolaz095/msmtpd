08cbea31e8b8d76bc7fdbc832caadd54ff83127b
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2023-08-06 14:33:35 +0300

    improve tests

--- plugins/helo/deny_rdns_mismatch_test.go
+++ plugins/helo/deny_rdns_mismatch_test.go
@@ -31,7 +31,7 @@ func TestDenyReverseDNSMismatch(t *testing.T) {
 			IP:       net.TCPAddr{IP: []byte{193, 41, 76, 25}, Port: 25},
 			PTRs:     []string{"r193-41-76-25.utex-telecom.ru."},
 			Helo:     "193.41.76.25",
-			ErrorMsg: "",
+			ErrorMsg: testErrorMessage,
 		},
 		{
 			IP:       net.TCPAddr{IP: []byte{213, 180, 204, 89}, Port: 25},
@@ -47,8 +47,29 @@ func TestDenyReverseDNSMismatch(t *testing.T) {
 		},
 		{
 			IP:       net.TCPAddr{IP: []byte{54, 240, 4, 12}, Port: 25},
-			PTRs:     nil,
+			PTRs:     []string{"a4-12.smtp-out.eu-west-1.amazonses.com."},
 			Helo:     "a4-12.smtp-out.eu-west-1.amazonses.com.",
+			ErrorMsg: "",
+		},
+		{
+			IP:       net.TCPAddr{IP: []byte{91, 239, 5, 18}, Port: 25},
+			PTRs:     nil,
+			Helo:     "mail.astralnalog.ru.",
+			ErrorMsg: testErrorMessage,
+		},
+		{
+			IP:       net.TCPAddr{IP: []byte{91, 239, 5, 18}, Port: 25},
+			PTRs:     nil,
+			Helo:     "mail.astral.ru.",
+			ErrorMsg: testErrorMessage,
+		},
+		{
+			IP: net.TCPAddr{IP: []byte{91, 239, 5, 18}, Port: 25},
+			PTRs: []string{
+				"mail.astralnalog.ru.",
+				"mail.astral.ru.",
+			},
+			Helo:     "mail.astral.ru.",
 			ErrorMsg: "",
 		},
 	}
--- plugins/helo/helpers_test.go
+++ plugins/helo/helpers_test.go
@@ -1,9 +1,11 @@
 package helo
 
 import (
+	"fmt"
 	"net"
 	"net/smtp"
 	"testing"
+	"time"
 
 	"github.com/vodolaz095/msmtpd"
 )
@@ -15,13 +17,25 @@ type testCase struct {
 	ErrorMsg string
 }
 
+func (tc *testCase) String() string {
+	return fmt.Sprintf("CASE: IP=%s PTRs=%v HELO=%s ERR=%s",
+		tc.IP.String(),
+		tc.PTRs,
+		tc.Helo,
+		tc.ErrorMsg,
+	)
+}
+
 func heloTestRunner(t *testing.T, cases []testCase, checkers []msmtpd.HelloChecker) {
 	for k := range cases {
 		addr, closer := msmtpd.RunTestServerWithoutTLS(t, &msmtpd.Server{
 			ConnectionCheckers: []msmtpd.ConnectionChecker{
 				func(tr *msmtpd.Transaction) error {
 					tr.Addr = &cases[k].IP
-					tr.PTRs = []string{cases[k].Helo}
+					//					tr.PTRs = []string{cases[k].Helo}
+					if len(cases[k].PTRs) > 0 {
+						tr.PTRs = cases[k].PTRs
+					}
 					return nil
 				},
 			},
@@ -35,7 +49,7 @@ func heloTestRunner(t *testing.T, cases []testCase, checkers []msmtpd.HelloCheck
 		err = c.Hello(cases[k].Helo)
 		if cases[k].ErrorMsg == "" {
 			if err != nil {
-				t.Errorf("FAIL: %s : unexpected error for testing %v - %v", err, k, cases[k])
+				t.Errorf("FAIL: %s : unexpected error for testing %v - %s", err, k, cases[k].String())
 			}
 		} else {
 			if err != nil {
@@ -45,14 +59,14 @@ func heloTestRunner(t *testing.T, cases []testCase, checkers []msmtpd.HelloCheck
 					t.Logf("error %s thrown as expected for %v", err.Error(), cases[k])
 				}
 			} else {
-				t.Errorf("FAIL: expected error %s is not thrown for %v - %v", cases[k].ErrorMsg, k, cases[k])
+				t.Errorf("FAIL: expected error %s is not thrown for %v - %s", cases[k].ErrorMsg, k, cases[k].String())
 			}
 		}
 		err = c.Close()
 		if err != nil {
-			t.Errorf("FAIL: %s : while closing connection for %v", err, k)
+			t.Errorf("FAIL: %s : while closing connection for %v %s", err, k, cases[k].String())
 		}
+		time.Sleep(100 * time.Millisecond)
 		closer()
 	}
-
 }
